b2fe2525cfba08dc256e4c94fe87cfc1216f5755
leng <lengbuntith2000@gmail.com>
2021-08-15 23:32:07 +0700

    kk

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f276112245ef9c7718d600efb44c27c8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c9fb4a60-e9c1-4d96-a0ca-e2fb3e7b126c'
+};
